1ff06e05516de7d53a26c9f35946183ceef84c26
MOISES LORAICO LOPEZ <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>
2023-07-27 02:32:29 -0500

    updated

--- fleet_urban_bus/controller/api.py
+++ fleet_urban_bus/controller/api.py
@@ -7,7 +7,7 @@ import logging
 from datetime import date, datetime, timedelta
 from psycopg2 import IntegrityError
 from odoo import http, SUPERUSER_ID
-from odoo.http import request,jsonify
+from odoo.http import request
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.translate import _
 from odoo.exceptions import ValidationError, Warning, UserError
@@ -16,30 +16,27 @@ from odoo.addons.base.models.ir_qweb_fields import nl2br
 _logger = logging.getLogger(__name__)
 
 
-class ApiWebService(http.Controller):
-	
-
-	@http.route('/api/webservice/en_bus_lines/', type='http', auth="public", methods=['GET'],csrf=False)
-	def api_licitacionadvancelineareport_post(self, *args, **kw):
-		try:
-			user = kw['UserName']
-			password = kw['Password']
-			data = []
-			if user == 'API_bus_urban_User' and password == 'Api$pass&256Gt4tHE63':			
-				sql = """SELECT * FROM fleet_vehicle""" 
-				request.env.cr.execute(sql)
-				data = request.env.cr.dictfetchall()
-				return jsonify(data)
-			else:
-				rpta = {
-					'status':'Error Data',
-					'log':'Datos de Conexion erroneos',
-				}
-				return jsonify(rpta)
-		except Exception as err:
-			rpta = {
-				'status':'Error Process',
-				'log':'Datos de Conexion erroneos',
-				'detail': str(err),
-			}
-			return jsonify(rpta)
+class ApiController(http.Controller):
+    @http.route('/api/webservice/en_bus_lines/', type='http', auth="public", methods=['GET'], csrf=False)
+    def api_licitacionadvancelineareport_post(self, *args, **kw):
+        try:
+            user = kw.get('UserName')
+            password = kw.get('Password')
+            if user == 'API_bus_urban_User' and password == 'Api$pass&256Gt4tHE63':
+                sql = """SELECT * FROM fleet_vehicle""" 
+                request.env.cr.execute(sql)
+                data = request.env.cr.dictfetchall()
+                return Response(json.dumps(data), content_type='application/json;charset=utf-8', status=200)
+            else:
+                rpta = {
+                    'status': 'Error Data',
+                    'log': 'Datos de Conexion erroneos',
+                }
+                return Response(json.dumps(rpta), content_type='application/json;charset=utf-8', status=400)
+        except Exception as err:
+            rpta = {
+                'status': 'Error Process',
+                'log': 'Datos de Conexion erroneos',
+                'detail': str(err),
+            }
+            return Response(json.dumps(rpta), content_type='application/json;charset=utf-8', status=500)
