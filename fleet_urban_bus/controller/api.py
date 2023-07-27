7998a6e38d1ea873a61616f18dc44057689c7831
MOISES LORAICO LOPEZ <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>
2023-07-27 02:37:42 -0500

    updated

--- fleet_urban_bus/controller/api.py
+++ fleet_urban_bus/controller/api.py
@@ -16,27 +16,30 @@ from odoo.addons.base.models.ir_qweb_fields import nl2br
 _logger = logging.getLogger(__name__)
 
 
-class ApiController(http.Controller):
-    @http.route('/api/webservice/en_bus_lines/', type='http', auth="public", methods=['GET'], csrf=False)
-    def api_licitacionadvancelineareport_post(self, *args, **kw):
-        try:
-            user = kw.get('UserName')
-            password = kw.get('Password')
-            if user == 'API_bus_urban_User' and password == 'Api$pass&256Gt4tHE63':
-                sql = """SELECT * FROM fleet_vehicle""" 
-                request.env.cr.execute(sql)
-                data = request.env.cr.dictfetchall()
-                return Response(json.dumps(data), content_type='application/json;charset=utf-8', status=200)
-            else:
-                rpta = {
-                    'status': 'Error Data',
-                    'log': 'Datos de Conexion erroneos',
-                }
-                return Response(json.dumps(rpta), content_type='application/json;charset=utf-8', status=400)
-        except Exception as err:
-            rpta = {
-                'status': 'Error Process',
-                'log': 'Datos de Conexion erroneos',
-                'detail': str(err),
-            }
-            return Response(json.dumps(rpta), content_type='application/json;charset=utf-8', status=500)
+class ApiWebService(http.Controller):
+	
+
+	@http.route('/api/webservice/en_bus_lines/', type='http', auth="public", methods=['GET'],csrf=False)
+	def api_licitacionadvancelineareport_post(self, *args, **kw):
+		try:
+			user = kw['UserName']
+			password = kw['Password']
+			data = []
+			if user == 'API_bus_urban_User' and password == 'Api$pass&256Gt4tHE63':			
+				sql = """SELECT * FROM fleet_vehicle""" 
+				request.env.cr.execute(sql)
+				data = request.env.cr.dictfetchall()
+				return str(data)
+			else:
+				rpta = {
+					'status':'Error Data',
+					'log':'Datos de Conexion erroneos',
+				}
+				return str(rpta)
+		except Exception as err:
+			rpta = {
+				'status':'Error Process',
+				'log':'Datos de Conexion erroneos',
+				'detail': str(err),
+			}
+			return str(rpta)
